58810180e61bbed6edebb34cb9a3852607801aa8
Allen Mwandunga <allenmcndounger@gmail.com>
2019-01-14 20:12:10 +0300

    Implementing ListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule,
+         MatFormFieldModule,
+         MatInputModule,
+         MatOptionModule,
+         MatSelectModule,
+         MatIconModule,
+         MatButtonModule,
+         MatCardModule,
+         MatTableModule,
+         MatDividerModule,
+         MatSnackBarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -29,7 +39,17 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSnackBarModule
   ],
   providers: [IssueService],
   bootstrap: [AppComponent]
